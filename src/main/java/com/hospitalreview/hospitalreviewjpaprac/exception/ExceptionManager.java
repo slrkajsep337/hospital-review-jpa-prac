b8199406bb12b2094bc646adc2ec0749d1f5ad7d
이수진 <slrkajsep337@naver.com>
2022-12-06 12:31:01 +0900

    exceptionHandler 구현

---
+++ src/main/java/com/hospitalreview/hospitalreviewjpaprac/exception/ExceptionManager.java
@@ -0,0 +1,17 @@
+package com.hospitalreview.hospitalreviewjpaprac.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionManager {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+}
