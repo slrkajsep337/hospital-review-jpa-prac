e0a11885c9bfd1b11679cecd59dd511735a3447b
이수진 <slrkajsep337@naver.com>
2022-12-06 12:24:44 +0900

    Initial commit

---
+++ src/main/java/com/hospitalreview/hospitalreviewjpaprac/HospitalReviewJpaPracApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalreview.hospitalreviewjpaprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalReviewJpaPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalReviewJpaPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospitalreview/hospitalreviewjpaprac/HospitalReviewJpaPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalreview.hospitalreviewjpaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalReviewJpaPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
